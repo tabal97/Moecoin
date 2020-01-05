96ab8317a8b7caa871da37faf3237616de670ed2
Mohamed <mr.tabal@yahoo.co.uk>
2020-01-05 18:04:01 +0000

    check validity of blockchain

--- main.js
+++ main.js
@@ -6,10 +6,52 @@ class Block {
         this.timestamp = timestamp;
         this.data = data;
         this.prevHash = prevHash;
-        this.hash = "";
+        this.hash = this.calculateHash();
     }
 
     calculateHash() {
-
+        return SHA256(this.index + this.prevHash + this.timestamp + JSON.stringify(this.data)).toString();
     }
 }
+
+class Blockchain {
+    constructor() {
+        this.chain = [this.createGenesisBlock];
+    }
+    createGenesisBlock() {
+        return new Block(0, "01/01/2020", "Genesis Block", "0")
+    }
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock) {
+        newBlock.prevHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+
+    isChainValid() {
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+            if (currentBlock.prevHash !== previousBlock.hash) {
+                return false;
+            }
+        }
+        return true
+    }
+}
+
+let moeCoin = new Blockchain();
+moeCoin.addBlock(new Block(1, "02/01/2020", { amount: 10000 }))
+moeCoin.addBlock(new Block(2, "02/01/2020", { amount: 100 }))
+moeCoin.addBlock(new Block(3, "04/01/2020", { amount: 999 }))
+
+// console.log(JSON.stringify(moeCoin, null, 4))
+
+console.log("Is Block chain valid?", moeCoin.isChainValid())
